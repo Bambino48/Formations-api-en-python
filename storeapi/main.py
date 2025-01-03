6eeb1f05be6c71e2b03e7f971fd313af5495633e
Bambino48 <ibamba366@gmail.com>
2025-01-03 10:05:37 +0000

    adding post

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,9 +1,21 @@
-import os
-
 from fastapi import FastAPI # type: ignore
+from storeapi.models.post import UserPost, UserPostIn
 
 app = FastAPI()
 
-@app.get("/") # api.com/
-async def root():
-    return {"message": "Hello, world!"}
+   
+post_table = {}
+
+
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@app.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
